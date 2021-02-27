4b267d246ffe4b3ca81eb7e22564a2ac44c42039
Justin Varghese <69856008+Justinnn07@users.noreply.github.com>
2021-02-27 17:54:38 +0530

    Update FaceDetector.py

--- FaceDetector.py
+++ FaceDetector.py
@@ -1,6 +1,11 @@
 # Importing cv2 dependency
 import cv2 
 from random import randrange
+import os
+import sys
+import subprocess
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Importing Image
 # img = cv2.imread("Images.jpg")
@@ -20,11 +25,17 @@ while True:
      cv2.rectangle(frame,(x, y), (x+w, y+h), (randrange(128,256), randrange(256), 0), 4)
 # To show
     cv2.imshow("Justin's Face detector", frame)
+    
 
 # to play
     key = cv2.waitKey(1)
     if key==81 or key==113:
         break
 webcam.release()
+
+try:
+    subprocess.Popen('explorer "Bill Records"')
+except Exception as e:
+    print(e)
 
 print("CODE COMPLETED")
